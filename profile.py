8c32863915cda93d1dff3d4528ef5b5d2154841a
Giovanni Romano <114951288+gromano02@users.noreply.github.com>
2024-10-26 19:45:05 -0400

    Add files via upload

--- profile.py
+++ profile.py
@@ -1,7 +1,5 @@
-"""This is a trivial example of a gitrepo-based profile; The profile source code and other software, documentation, etc. are stored in in a publicly accessible GIT repository (say, github.com). When you instantiate this profile, the repository is cloned to all of the nodes in your experiment, to `/local/repository`. 
-
-This particular profile is a simple example of using a single raw PC. It can be instantiated on any cluster; the node will boot the default operating system, which is typically a recent version of Ubuntu.
-
+"""
+mydpdkndns setup
 Instructions:
 Wait for the profile instance to start, then click on the node in the topology and choose the `shell` menu item. 
 """
@@ -16,12 +14,19 @@ pc = portal.Context()
 
 # Create a Request object to start building the RSpec.
 request = pc.makeRequestRSpec()
- 
-# Add a raw PC to the request.
-node = request.RawPC("node")
 
-# Install and execute a script that is contained in the repository.
-node.addService(pg.Execute(shell="sh", command="/local/repository/setup.sh"))
+# Add two raw PCs to the request.
+node1 = request.RawPC("node1")
+node2 = request.RawPC("node2")
+
+# Connect the two nodes.
+link = request.Link("link")
+link.addInterface(node1.addInterface("eth1"))
+link.addInterface(node2.addInterface("eth1"))
+
+# Install and execute a script on both nodes that is contained in the repository.
+node1.addService(pg.Execute(shell="sh", command="/local/repository/setup.sh"))
+node2.addService(pg.Execute(shell="sh", command="/local/repository/setup2.sh"))
 
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
