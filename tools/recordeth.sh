266c47c2f9b9a79bf19fde3c04596f7228521381
Giovanni Romano <114951288+gromano02@users.noreply.github.com>
2024-10-27 04:06:17 -0400

    Update recordeth.sh

--- tools/recordeth.sh
+++ tools/recordeth.sh
@@ -3,25 +3,29 @@
 dev=$1
 
 record_eth() {
-    inetinfo=`ifconfig $dev | grep -w inet`
-    macinfo=`ifconfig $dev | grep ether`
+    inetinfo=$(ifconfig $dev | grep -w inet)
+    macinfo=$(ifconfig $dev | grep ether)
 
-    ipaddr=`echo $inetinfo | awk '/inet/{print $2}'`
-    netmask=`echo $inetinfo | awk '/inet/{print $4}'`
-    mac=`echo $macinfo | awk '/ether/{print $2}'`
-    gateway=`route -n | awk '/UG/{print $2}'`
+    ipaddr=$(echo $inetinfo | awk '/inet/{print $2}')
+    netmask=$(echo $inetinfo | awk '/inet/{print $4}')
+    mac=$(echo $macinfo | awk '/ether/{print $2}')
+    gateway=$(route -n | awk '/UG/{print $2}')
 
     basepath=$(cd `dirname $0`; pwd)
-    nicinfo="$basepath/ethinfo
+    nicinfo="$basepath/ethinfo"
     rm -f $nicinfo
 
-    if [ -z "$ipaddr" -o -z "$netmask" -o -z "$mac" -o -z "$gateway" ]; then
+    if [ -z "$ipaddr" ] || [ -z "$netmask" ] || [ -z "$mac" ] || [ -z "$gateway" ]; then
         echo "Get Eth Info Failed" && exit 1
     fi
 
-    if [ ! -f $nicinfo ]; then
-        echo -e "DEV=$dev\nIPADDR=$ipaddr\nNETMASK=$netmask\nMAC=$mac\nGATEWAY=$gateway" > $nicinfo
-    fi
+    echo -e "DEV=$dev\nIPADDR=$ipaddr\nNETMASK=$netmask\nMAC=$mac\nGATEWAY=$gateway" > $nicinfo
 }
+
+# Check if a device name was provided
+if [ -z "$dev" ]; then
+    echo "Usage: $0 <network_device>"
+    exit 1
+fi
 
 record_eth $dev
